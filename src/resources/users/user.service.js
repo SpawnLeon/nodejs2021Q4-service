2d46399eaa135f3447dd1dd8b51b745ee3348b92
Alexander Leonov <spawnleon@gmail.com>
2021-12-03 19:39:47 +0300

    feat: add user service

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,22 @@
 const usersRepo = require('./user.memory.repository');
+const User = require('./user.model');
 
-const getAll = () => usersRepo.getAll();
+const getUsers = async () => usersRepo.getUsers();
+const createUser = async (userData) => {
+  const user = new User(...userData);
+  return usersRepo.createUser(user);
+};
+const getUser = async (id) => usersRepo.getUser(id);
+const updateUser = async (id, userData) => {
+  const user = await usersRepo.getUser(id);
+  if (!user) {
+    return null;
+  }
+  const updatedUser = { ...user, ...userData };
+  return usersRepo.updateUser(updatedUser);
+};
+const deleteUser = async (id) => usersRepo.deleteUser(id);
 
-module.exports = { getAll };
+module.exports = {
+  getUsers, createUser, getUser, updateUser, deleteUser,
+};
