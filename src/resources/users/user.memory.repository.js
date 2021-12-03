c2c7080d2b5b577e9c803b8a06fae6ce47a5580c
Alexander Leonov <spawnleon@gmail.com>
2021-12-03 19:38:18 +0300

    feat: add user repo

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,25 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const USERS = [];
+
+const getUsers = async () => USERS;
+
+const createUser = async (user) => {
+  USERS.push(user);
+  return user;
 };
 
-module.exports = { getAll };
+const getUser = async (id) => USERS.find((user) => user.id === id);
+
+const updateUser = async (user) => {
+  const index = USERS.indexOf(user);
+  USERS[index] = user;
+};
+
+const deleteUser = async (id) => {
+  const user = await getUser(id);
+  const index = USERS.indexOf(user);
+  USERS.splice(index, 1);
+};
+
+module.exports = {
+  getUsers, createUser, getUser, updateUser, deleteUser,
+};
